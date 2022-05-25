16c0a75f1002fbdc06240123c78ee5fe129ee694
jake jordan <jakesjordan11@gmail.com>
2022-05-24 19:55:38 -0600

    continuing creation of content on site

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,11 @@
 import { Link } from "@remix-run/react";
 import React from "react";
-import jakesjordan from "~/../public/images/jakesjordan.png";
-import resume from "~/../public/jakesjordan resume.png";
-import logo from "~/../public/svg/Practice_logo.svg";
+import jakesjordan from "~/../public/assets/jakesjordan.png";
+import resume from "~/../public/assets/jakesjordan resume.png";
+import audioseiumLogo from "~/../public/assets/audioseium.svg";
+import rvneLogo from "~/../public/assets/abstract 1.png";
+import mobileLogo from "~/../public/assets/abstract 2.png";
+import jakeLogo from "~/../public/assets/jakesjordan.svg";
 
 export default function Index() {
   return (
@@ -25,9 +28,9 @@ export default function Index() {
                 stroke="currentColor"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M4 6h16M4 12h16M4 18h16"
                 />
               </svg>
@@ -49,64 +52,87 @@ export default function Index() {
           <p>creator</p>
         </div>
       </header>
-      <h2 className="absolute bottom-4 px-8 text-2xl font-medium border-b-4">
-        Projects
-      </h2>
-      <main className="h-screen">
-        <section className="flex flex-col justify-center px-12 pb-8 space-y-32">
-          <article className="px-8 rounded-2xl shadow-lg">
-            <header className="py-4">
-              <h3 className="text-xl font-bold text-center border-b-2">RVNE</h3>
-            </header>
-            <figure className="">
-              <img src={logo} alt=""></img>
-            </figure>
-            <footer className="py-4 text-xl text-center">
-              a parametric flow based programing tool on the web
-            </footer>
-          </article>
-
-          <article className="px-8 rounded-2xl shadow-lg">
-            <header className="py-4">
-              <h3 className="text-xl font-bold text-center border-b-2">
-                Audioseium
-              </h3>
-            </header>
-            <figure className="">
-              <img src={logo} alt=""></img>
-            </figure>
-            <footer className="py-4 text-xl text-center">
-              a library of my personal music
-            </footer>
-          </article>
-
-          <article className="px-8 rounded-2xl shadow-lg">
-            <header className="py-4">
-              <h3 className="text-xl font-bold text-center border-b-2">
-                Personal Site
-              </h3>
-            </header>
-            <figure className="">
-              <img src={logo} alt=""></img>
-            </figure>
-            <footer className="py-4 text-xl text-center">
-              my website showcasing my projects and interests
-            </footer>
-          </article>
-
-          <article className="px-8 rounded-2xl shadow-lg">
-            <header className="py-4">
-              <h3 className="text-xl font-bold text-center border-b-2">
-                Mobile Project
-              </h3>
-            </header>
-            <figure className="">
-              <img src={logo} alt=""></img>
-            </figure>
-            <footer className="py-4 text-xl text-center">
-              options: react native design system, plaid api project
-            </footer>
-          </article>
+      <main>
+        <section className="flex flex-col justify-center pb-16">
+          <header>
+            <h2 className="absolute left-0 bottom-4 px-8 text-2xl font-medium border-b-4">
+              Projects
+            </h2>
+          </header>
+          <div className="px-12 space-y-32">
+            <article className="px-8 rounded-2xl shadow-lg">
+              <header className="py-4">
+                <h3 className="text-xl font-bold text-center border-b-2">
+                  RVNE
+                </h3>
+              </header>
+              <figure>
+                <img src={rvneLogo} alt=""></img>
+              </figure>
+              <footer className="py-4 text-xl text-center">
+                a parametric flow based programing tool on the web
+              </footer>
+            </article>
+            <article className="px-8 rounded-2xl shadow-lg">
+              <header className="py-4">
+                <h3 className="text-xl font-bold text-center border-b-2">
+                  Audioseium
+                </h3>
+              </header>
+              <figure>
+                <img src={audioseiumLogo} alt=""></img>
+              </figure>
+              <footer className="py-4 text-xl text-center">
+                a library of my personal music
+              </footer>
+            </article>
+            <article className="px-8 rounded-2xl shadow-lg">
+              <header className="py-4">
+                <h3 className="text-xl font-bold text-center border-b-2">
+                  Personal Site
+                </h3>
+              </header>
+              <figure>
+                <img src={jakeLogo} alt=""></img>
+              </figure>
+              <footer className="py-4 text-xl text-center">
+                my website showcasing my projects and interests
+              </footer>
+            </article>
+            <article className="px-8 rounded-2xl shadow-lg">
+              <header className="py-4">
+                <h3 className="text-xl font-bold text-center border-b-2">
+                  Mobile Project
+                </h3>
+              </header>
+              <figure>
+                <img src={mobileLogo} alt=""></img>
+              </figure>
+              <footer className="py-4 text-xl text-center">
+                options: react native design system, plaid api project
+              </footer>
+            </article>
+          </div>
+        </section>
+        <section className="flex flex-col justify-center pb-32">
+          <header className="pb-32">
+            <h2 className="px-8 w-min text-2xl font-medium border-b-4">
+              Interests
+            </h2>
+          </header>
+          <div className="px-12 space-y-32">
+            <article className="px-8 rounded-2xl shadow-lg">
+              <header className="py-4">
+                <h3 className="text-xl font-bold text-center border-b-2">
+                  Blender
+                </h3>
+              </header>
+              <figure className="">
+                <img src={audioseiumLogo} alt=""></img>
+              </figure>
+              <footer className="py-4 text-xl text-center">3d Mo</footer>
+            </article>
+          </div>
         </section>
         <section>
           <h2 className="px-8 w-min text-2xl font-medium border-b-4">Resume</h2>
